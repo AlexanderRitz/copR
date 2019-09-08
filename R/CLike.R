350bcd14157813caf4848e0180540ff4c5e8dab5
Alexander Ritz <alex.ritz92@googlemail.com>
2019-09-08 06:02:07 +0200

    Likelihood function initialisation

---
+++ R/CLike.R
@@ -0,0 +1,20 @@
+#' Likelihood of Copula
+#'
+#' Calculates the Likelihood function of a chosen Copula family
+#'
+#' @param copula ArCop object. Decides the copula family for which the Likelhood function is to be calculated.
+#' @param marg character. Supplied choices for the marginal distribution functions.
+#' Can either be estimated through empirical functions or given by name.
+#' @return An expression of the calculated Likelihood function.
+#'
+#' @examples
+#' \donttest{
+#' exC <- copu(Type = "Clayton", par = 5, dim = 2)
+#' exS <- rCop(copula = exC, n = 1000)
+#' }
+#'
+#' @export
+
+CLike <- function(copula, marg){
+  print("Initialisation")
+}
