5a99861e041cfb3a3988b463c2cda3c979e6c628
Juliane Wilcke <juliane.wilcke@gmail.com>
2019-09-16 01:05:59 +0200

    Set up vignette

--- R/coppersplot.R
+++ R/coppersplot.R
@@ -6,19 +6,11 @@
 #' @param FUN function. Gives the function to be plotted.
 #' @param n.grid integer.
 #' @param delta numeric.
+#' @param nlevels integer.
 #' @param xlim integer.
 #' @param ylim integer.
-#' @param zlim integer.
 #' @param xlab character.
 #' @param ylab character.
-#' @param zlab character.
-#' @param theta integer.
-#' @param phi integer.
-#' @param expand integer.
-#' @param col character, function, or integer.
-#' @param shade integer.
-#' @param ticktype character.
-#'
 #' @return A perspective plot of a surface over the x-y plane using the values given by FUN for a copula of the form given by the supplied copula object.
 #'
 #' @examples
@@ -38,13 +30,13 @@ coppersplot <- function (copula,
                          zlim = NULL,
                          xlab = "u1",
                          ylab = "u2",
-                         zlab = deparse(substitute(FUN))[1],
                          theta = -45/2,
                          phi = 30,
                          expand = 0.75,
-                         col = heat.colors,
+                         col = c("red", "yellow"),
                          shade = 0.2,
                          ticktype = "detail",
+                         zlab = deparse(substitute(FUN))[1],
                          ...) {
   if (copula$dimension == 2 & n.grid >= 2) {
     if (length(n.grid) == 1) {
@@ -66,6 +58,13 @@ coppersplot <- function (copula,
           if (min(zlim, na.rm = TRUE) == max(zlim, na.rm = TRUE)) {
             stop("Perspective plots of a horizontal plane are not supported.")
           }
+        }
+        if (length(col) == 2) {
+          colpal <- colorRampPalette(col)
+          cols <- colpal(100)
+          z.facet.center <- (z[-1, -1] + z[-1, -ncol(z)] + z[-nrow(z), -1] + z[-nrow(z), -ncol(z)])/4
+          z.facet.range <- cut(z.facet.center, 100)
+          col <- cols[z.facet.range]
         }
         graphics::persp(
           x = gx,
